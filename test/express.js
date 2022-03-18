ed5680e116361d0801a0a20d60adb95e4a66cda4
Jorgen Evens <jorgen@evens.eu>
2022-03-18 11:56:08 +0100

    feat(express): add extraction of hostname

--- test/express.js
+++ test/express.js
@@ -61,4 +61,85 @@ describe('Express', function () {
         })
     })
 
+    describe('host', function() {
+        it('should extract the host from a request', function () {
+            var req = {
+                protocol: 'https',
+                baseUrl: '/test',
+                url: '/index.html',
+                get: k => {
+                    if (k == 'host')
+                        return 'localhost:3000';
+                }
+            };
+
+            var result = URL.express.host(req);
+            assert.equal(result, 'localhost:3000');
+        })
+    })
+    
+    describe('hostname', function() {
+        it('should extract the hostname from a request', function () {
+            var req = {
+                protocol: 'https',
+                baseUrl: '/test',
+                url: '/index.html',
+                get: k => {
+                    if (k == 'host')
+                        return 'localhost:3000';
+                }
+            };
+
+            var result = URL.express.hostname(req);
+            assert.equal(result, 'localhost');
+        })
+        
+        it('should extract the hostname from a request with IP', function () {
+            var req = {
+                protocol: 'https',
+                baseUrl: '/test',
+                url: '/index.html',
+                get: k => {
+                    if (k == 'host')
+                        return '127.0.0.1:3000';
+                }
+            };
+
+            var result = URL.express.hostname(req);
+            assert.equal(result, '127.0.0.1');
+        })
+        
+        it('should extract the hostname from a request with only IP', function () {
+            var req = {
+                protocol: 'https',
+                baseUrl: '/test',
+                url: '/index.html',
+                get: k => {
+                    if (k == 'host')
+                        return '127.0.0.1';
+                }
+            };
+
+            var result = URL.express.hostname(req);
+            assert.equal(result, '127.0.0.1');
+        })
+    })
+    
+    describe('protocol', function() {
+        it('should extract the protocol from a request', function () {
+            var req = {
+                protocol: 'https',
+                baseUrl: '/test',
+                url: '/index.html',
+                get: k => {
+                    if (k == 'host')
+                        return 'localhost:3000';
+                }
+            };
+
+            var result = URL.express.protocol(req);
+            assert.equal(result, 'https');
+        })
+    })
+
 })
